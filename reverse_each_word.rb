fc2edb95c1eec65a1cf34affdb0ef9506292e1de
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2021-01-27 00:13:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,8 @@ string_to_array.collect do |element|
   
 end
 end
+def reverse_each_word( string )
+   string_to_array = string.split ( " " )
+   sentence_array = string_to_array.collect{ | element | element.reverse }
+   sentence_array.join( " " )
+end
