53eff222399f8af46a17d20873b540db1bb1a729
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2021-01-27 00:12:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(string)
 string_to_array = string.split (" ")
 string_to_array.collect do |element|
   element.reverse
-  sentence_array.join( " " )
+  element.join( " " )
 end
 end
